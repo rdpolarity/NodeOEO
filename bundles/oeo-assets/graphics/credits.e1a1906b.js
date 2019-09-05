10e9437aac3ce45ce92ec8eddbde341bf88e6cc0
rdpolarity <rdpolarity@gmail.com>
2019-09-05 20:26:11 +1000

    update credits

--- bundles/oeo-assets/graphics/credits.e1a1906b.js
+++ bundles/oeo-assets/graphics/credits.e1a1906b.js
@@ -77226,7 +77226,7 @@ module.exports = [{
   "players": ["RDPolarity", "vaZ", "FiLeZekk", "Chung"]
 }, {
   "title": "Sponsors",
-  "players": ["For business inquires! Email: OceanicEsportsOpen@gmail.com"]
+  "players": ["For business inquiries, contact OceanicEsportsOpen@gmail.com"]
 }];
 },{}],"../../node_modules/react-particles-js/lib/particles.js":[function(require,module,exports) {
 var define;
@@ -77417,7 +77417,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "60041" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "60894" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
--- bundles/oeo-assets/graphics/gameplay.7786e409.js
+++ bundles/oeo-assets/graphics/gameplay.7786e409.js
@@ -76654,7 +76654,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "60041" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "60894" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
